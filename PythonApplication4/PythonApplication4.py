b645ef996e521f77e1174fca3f7f9fdfbc4bcc9c
MayukoOzaki <22011077mo@tama.ac.jp>
2021-04-19 19:43:22 +0900

    グラフ(8)

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -3,18 +3,15 @@ import struct
 import matplotlib.pyplot as plt
 
 ser = serial.Serial("COM4",115200,timeout = 1.0)
-b_reply = ser.read(230) 
+b_reply = ser.readline()
+#print(b_reply)
 #b_reply = ser.read_all()
-
-print(b_reply)
-#reply =struct.unpack('230B',b_reply)
 reply=b_reply.decode()
 print(reply)
 ser.close()
-#reply=list(reply)
-reply=reply.split("\r\n")
-print(reply)
+
 replyli=list(reply)
+print(replyli)
 
 
 import time
@@ -23,15 +20,15 @@ import time
 #bre_ave=sum(replyli)/len(replyli)
 #bre.append(bre_ave)
 
-bre=[]
+"""bre=[]
 for a in range(0,10):
-    bre_ele=replylist[0+(23*a)-4:4+(23*a)] #23個ずつ取り出す0.1秒
-    bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
-    bre.append(bre_ave)
-
+   bre_ele=replylist[0+(23*a)-4:4+(23*a)] #23個ずつ取り出す0.1秒
+   bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
+   bre.append(bre_ave)
 t=[ 0.1*i for i in range(len(bre)]
 plt.plot(t,bre,label="signal")
 plt.show()
+"""
 
 
 
