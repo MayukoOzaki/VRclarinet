728acfe6155cf633368de813eda6bc454e5f2f31
MayukoOzaki <22011077mo@tama.ac.jp>
2021-04-19 19:14:51 +0900

    グラフ(7)

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -20,12 +20,16 @@ replyli=list(reply)
 import time
 
 
+#bre_ave=sum(replyli)/len(replyli)
+#bre.append(bre_ave)
+
 bre=[]
-bre_ave=sum(replyli)/len(replyli)
-bre.append(bre_ave)
+for a in range(0,10):
+    bre_ele=replylist[0+(23*a)-4:4+(23*a)] #23個ずつ取り出す0.1秒
+    bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
+    bre.append(bre_ave)
 
-
-t=[i for z in range(len(bre)]
+t=[ 0.1*i for i in range(len(bre)]
 plt.plot(t,bre,label="signal")
 plt.show()
 
@@ -34,15 +38,6 @@ plt.show()
 # starttime = time.time()  #現在時刻
 #  t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
 #  for a in range(0,10):
-#  bre_ele=replylist[0(+a*5)-4:4+(5*a)] #5個ずつ取り出す0-4 5-9 10-14
+#  bre_ele=replylist[0+(5*a):4+(5*a)] #5個ずつ取り出す0-4 5-9 10-14
 #  bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
-#  bre.append(bre_ave)"""
-
-
-       
-
-    
-    
-
-
-
+#  bre.append(bre_ave)
