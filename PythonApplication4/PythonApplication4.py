64ace634d8373eac37e53b6a4e7c57c466287833
MayukoOzaki <22011077mo@tama.ac.jp>
2021-04-19 18:37:22 +0900

    グラフ(5)

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -1,12 +1,12 @@
 ﻿import serial
 import struct 
 
-ser = serial.Serial("COM4",115200,timeout = 0.1)
-b_reply = ser.read(23) 
+ser = serial.Serial("COM4",115200,timeout = 1.0)
+b_reply = ser.read(230) 
 #b_reply = ser.read_all()
 
 print(b_reply)
-#reply =struct.unpack('23B',b_reply)
+#reply =struct.unpack('230B',b_reply)
 reply=b_reply.decode()
 print(reply)
 ser.close()
@@ -18,17 +18,25 @@ replyli=list(reply)
 
 import time
 
-starttime = time.time()  #現在時刻
-t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
+#starttime = time.time()  #現在時刻
+#t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
 bre=[]
-for a in range(0,10):
+bre_ave=sum(replyli)/len(replyli)
+bre.append(bre_ave)
+
+t=[i for z in range(len(bre) ]
+plt.plot(t,bre,label='signal')
+plt.show()
+
+"""
+    starttime = time.time()  #現在時刻
+    t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
+
+    for a in range(0,10):
     bre_ele=replylist[0(+a*5)-4:4+(5*a)] #5個ずつ取り出す0-4 5-9 10-14
     bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
-    bre.append(bre_ave)
+    bre.append(bre_ave)"""
 
-
-plt.plot(t, label='signal')
-plt.show()
 
        
 
