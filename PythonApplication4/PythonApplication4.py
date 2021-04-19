4da17c314c5887827e0c3072f61aeacea4bc4201
MayukoOzaki <22011077mo@tama.ac.jp>
2021-04-19 18:09:24 +0900

    グラフ(2)

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -11,14 +11,26 @@ reply=b_reply.decode()
 print(reply)
 ser.close()
 #reply=list(reply)
-print(reply.split("\r\n"))
+reply=reply.split("\r\n")
+print(reply)
+replyli=list(reply)
+
+
+import time
+
+starttime = time.time()  #現在時刻
+t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
+for a in range(0,10):
+    bre_ele=replylist[0(+a*5)-4:4+(5*a)] #5個ずつ取り出す0-4 5-9 10-14
+    bre_ave=sum(bre_ele)/len(bre_ele) #平均をとる
+
 
 
 
 plt.plot(t, label='signal')
 plt.show()
 
-        
+       
 
     
     
