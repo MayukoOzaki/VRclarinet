4d0ec415bef237b9ebdb0aa7d1bf4f644e342819
MayukoOzaki <22011077mo@tama.ac.jp>
2021-04-21 18:59:21 +0900

    リアルタイムグラフ(1)

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -1,20 +1,42 @@
 ﻿import serial
 import struct 
 import matplotlib.pyplot as plt
-
-ser = serial.Serial("COM4",115200,timeout = 1.0)
-reply = ser.readline()
-#print(reply)
-#print(type(reply)) byte
-reply=reply.decode()
-#print(type(reply)) str
-print(reply)
-ser.close()
-
+import numpy as np
 import time
 
-plt.plot(t,reply,label="signal")
-plt.show()
+
+starttime = time.time() #開始時刻
+reply_l=[] #流量データ
+t_l=[] #演奏時間
+
+
+while True:
+    import time
+    ser = serial.Serial("COM4",115200,timeout = 1.0)
+    reply = ser.readline() #byte
+    t = time.time() - starttime# 演奏時間(現在時刻-演奏開始時刻
+    reply=reply.decode() #str
+    reply=int(reply) #int 
+    print(reply)     
+    ser.close()
+
+    reply_l.append(reply) #reply_lに追加
+    if len(reply_l)==11: #reply_lが11以上だったら1番目を消す
+        del reply_l[1]
+    
+    t_l.append(t)#演奏時間をt_lに追加
+    if len(t_l)==11: #t_lが11以上だったら1番目を消す
+        del t_l[1]
+
+
+
+    fig, ax = plt.subplots(1, 1) # 描画領域を取得
+    line, = ax.plot(t_l[-1],reply_l[-1] , color='blue') #t_l,reply_lの最後尾を描写する
+    plt.pause(0.01)
+    line.remove()
+
+   # plt.plot(t,reply,label="signal")
+   # plt.show()
 
 
 
