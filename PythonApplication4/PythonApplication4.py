08726333ea584ebe27d29eb35ac034f0fefdc965
MayukoOzaki <22011077mo@tama.ac.jp>
2021-05-07 16:59:54 +0900

    流量計のデータから曲を進行

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import filedialog
 import msvcrt
-
 import os
 
 
@@ -15,37 +14,16 @@ import os
 #01:48～02:26
 #02:26～02:46
 
-# 仮想「息吹き込み検出関数」
-# "b"(吹く)が押されていれば 1.0（息の量） / "s"（止める）が押されていれば 0.0（息の量） を返す　
-def get_breath_speed(current_speed): 
-    breath_speed = current_speed
-    if msvcrt.kbhit():
-        ch = msvcrt.getch()
-        print(ch)
-        if ch==b'b': 
-            breath_speed=1.0
-        elif ch==b's': 
-            breath_speed=0.0
-        else:
-            msvcrt.ungetch(ch) #push back
-    return breath_speed
 
-# 仮想「」
-# ボタンが押されていると、True / 押されていないと False を返す
-def get_switch():
-    is_pressed = False
-    if msvcrt.kbhit():
-        ch = msvcrt.getch()
-        print(ch)
-        if ch==b' ':            #スペースの場合True 
-            is_pressed=True
-        else:
-            msvcrt.ungetch(ch)  #スペース以外の場合呼び出さなかったことにする
-    return is_pressed
+low=850
+def breath_detection(reply):
+    is_blowing = False
+    if reply<low:  #1000未満だったら吹いている
+         is_blowing=True
+    else:
+        is_blowing= False
+    return is_blowing
 
-
-# msvcrt.getch() は「この関数呼び出しは読み出し可能な打鍵がない場合にはブロックします」なので、getch() を呼ぶ前に kbhit() が必要
-# msvcrt.kbhit()読み出し待ちの打鍵イベントが存在する場合に True を返します。
 
 #MiDI読み込み
 
@@ -62,12 +40,12 @@ midi_data = pretty_midi.PrettyMIDI(fle)
 midi_tracks = midi_data.instruments
 # トラック１のノートを取得
 notes = midi_tracks[0].notes
-for note in notes:
+#for note in notes:
     # ベロシティー、ノートナンバー、
     # ノートオンタイム、ノートオフタイム
     # の順でノート情報が渡される
     # Note(start=50.731117, end=51.406388, pitch=78, velocity=100)
-    print(note)
+    #print(note)
 
 
 #リアルタイムで時間を図って音符を表示
@@ -85,48 +63,109 @@ nextnote2 = 0  # 次の Note のインデックス（演奏）
 starttime = time.time()  #現在時刻
 t = 0
 out=1          # 出力されたかどうか
-breath_old = 0 # 直前に息が吹き込まれていたか
+#breath_old = 0 # 直前に息が吹き込まれていたか
 
+
+       
+
+
+#今吹き込まれていて、直前が吹き込まれていなければ、次の音符を出し始め
+# 直前が吹き込まれていて、今吹き込まれていなければ、音止める
+
+breath_old=False 
+
+import serial
+
+ser = serial.Serial("COM4",115200,timeout = 1.0)
+
+while True:
+    reply = ser.readline() #byte
+    ser.reset_input_buffer()
+    try:
+        reply=reply.decode() #str
+        reply=int(reply) #int 
+    except:
+        continue
+
+    
+    t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
+    breath_now =  breath_detection(reply)
+    #print(breath_now,breath_old)
+    if breath_now == True and breath_old==False: 
+        while t>notes[nextnote2].end:
+            if nextnote2<len(notes)-1: #notesの範囲の一つ手前であればnextnoteを増やす
+                nextnote2+=1
+            else:
+                break
+        print(nextnote2,notes[nextnote2].pitch)          
+    elif breath_now == False and breath_old==True: #音を止めて次の音に切り替える
+        print(0)
+        nextnote2+=1  
+    breath_old = breath_now 
+
+   #もし、ボタンで切替or息を吹き始めた際に、次の音の演奏時間内であるかを確認、範囲外(後)であれその次の音を確認する。
+   #さらに過ぎていた場合これを繰り返す
+   #もし、ボタンで切替or息を吹き始めた際に、次の音の演奏時間内であるかを確認、範囲外(前)であれば、そのまま演奏。
+
+
+   #やること
+   #・音が高くなる程流量増える
+
+
+
+  
+   
+   
+"""
+# 仮想「息吹き込み検出関数」
+# # "b"(吹く)が押されていれば 1.0（息の量） / "s"（止める）が押されていれば 0.0（息の量） を返す　
+def get_breath_speed(current_speed): 
+    breath_speed = current_speed
+    if msvcrt.kbhit():
+        ch = msvcrt.getch()
+        print(ch)
+        if ch==b'b': 
+            breath_speed=1.0
+        elif ch==b's': 
+            breath_speed=0.0
+        else:
+            msvcrt.ungetch(ch) #push back
+    return breath_speed
+"""
+
+
+"""
+# 仮想「」
+# # ボタンが押されていると、True / 押されていないと False を返す
+def get_switch():
+    is_pressed = False
+    if msvcrt.kbhit():
+        ch = msvcrt.getch()
+        print(ch)
+        if ch==b' ':            #スペースの場合True 
+            is_pressed=True
+        else:
+            msvcrt.ungetch(ch)  #スペース以外の場合呼び出さなかったことにする
+    return is_pressed
+
+# msvcrt.getch() は「この関数呼び出しは読み出し可能な打鍵がない場合にはブロックします」なので、getch() を呼ぶ前に kbhit() が必要
+# msvcrt.kbhit()読み出し待ちの打鍵イベントが存在する場合に True を返します。
+"""
+
+"""
 while t < notes[-1].end  :
     t = time.time() - starttime # 演奏時間(現在時刻-演奏開始時刻)
 
     # キーが押されていたら次の音符を出す
-    if is_pressed == True:
-        print(notes[nextnote2].pitch)
-        nextnote2+=1
-
+   # if is_pressed == True:
+    #    print(notes[nextnote2])
+     #   nextnote2+=1
     # 見本
     if t>notes[nextnote1].start:
         if out==1:
-            print(notes[nextnote1].pitch) #音を鳴らす
+            print("見本",notes[nextnote1].pitch) #音を鳴らす
             out=0
     if t>notes[nextnote1].end:
         nextnote1+=1
         out=1
-       
-
-
-
-
-
-
-"""今吹き込まれていて、直前が吹き込まれていなければ、次の音符を出し始め
-    # 直前が吹き込まれていて、今吹き込まれていなければ、音止める
-    # # キーが押されていたら次の音符を出す
-    breath_now = get_breath_speed(breath_old)
-    if breath_now > 0:
-        if breath_old == 0:
-            print(notes[nextnote2].pitch)
-        if breath_now == 0:
-        if breath_old >0:
-            print(0)
-            nextnote2+=1
-    breath_old = breath_now """
-
-
-"""
-やること
-・採点（区分求積法）
-・ブレス
-息の
-"""
+        """
